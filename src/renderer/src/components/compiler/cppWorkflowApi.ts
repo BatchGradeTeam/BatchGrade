be8239a6b23312cbea893459b0c87bc1dc7f4888
HiMyNameisScott <50992532+HiMyNameisScott@users.noreply.github.com>
2026-03-26 16:44:18 -0700

    Update cppWorkflowApi.ts
    Just added some more comments to explain stuff inside of the api aspect.

--- src/renderer/src/components/compiler/cppWorkflowApi.ts
+++ src/renderer/src/components/compiler/cppWorkflowApi.ts
@@ -4,6 +4,15 @@ import type {
   RunCppResult
 } from '../../../../shared/compiler'
 
+// The point of this entire file is to act as a middle layer, this allows us passing items
+// from the cppworkflow to  the workflowapi, ultimately we will use these wrappers to do our bidding
+// this is essentially how we interact with everything on the machine.
+
+// A promise is a temporary place holder for a value we will get at a later time
+
+// Pretty much want to look at the window.api.xxxx this will essentially let you know what is going
+// on in this :)  
+ 
 export function getCompilerStatus(): Promise<GccInstallationInfo> {
   return window.api.compiler.getGccStatus()
 }
