5f966b85f5287ba313b55a65cf4e553f40dd7881
Truc Bui <bamboo.tru@gmail.com>
2026-03-26 16:41:17 -0700

    Change test coverage to per-file
    The 90% statement coverage is now per-file instead of a global
    average. Also updated some comments in ci.yml

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'html', 'json-summary'],
       thresholds: {
+        perFile: true,
         statements: 90,
       },
       exclude: [
