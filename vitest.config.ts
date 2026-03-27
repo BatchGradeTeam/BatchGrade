24114659a22b542114fd5ae15376e555d638f714
Truc Bui <bamboo.tru@gmail.com>
2026-03-26 17:44:24 -0700

    Test ci.yml v4 -> v5 & new coverage command
    Updated actions/checkout & actions/setup-node from v4 to v5
    and changed the Test step's command to test if the step fails
    in the CI pipeline when there are coverage errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'json-summary'],
+      enabled: true,
       thresholds: {
         perFile: true,
         statements: 90,
